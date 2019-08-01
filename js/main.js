515a7c4c36455c2111b06475b8ef60ac98989893
jessexknight <jesse.x.knight@gmail.com>
2019-07-31 22:34:22 -0400

    adding episode skeletons, major restyling & minor refactoring

--- js/main.js
+++ js/main.js
@@ -1,15 +1,19 @@
-
-$(document).ready(function(){
-  // setup the spotlight
-  var spotcon = $(".spotlight-container");
-  var spotlight = $("#spotlight");
-  $(".profile-img").click(function(){
-    spotlight.html($("#"+this.id+"-details").html());
-    spotcon.removeClass("hidden");
-  });
-  spotcon.find(".close").click(function(){spotcon.addClass("hidden");});
-  $(document).keyup(function(e){if (e.key === "Escape") {spotcon.addClass("hidden");}});
-});
+// TODO: since jQ is from cdn: no internet = stuck on loader :(
+// $(document).ready(function(){
+//   setup the profile-popup
+//   var modal = $("#modal-content");
+//   $(".modal-button").click(function(){
+//     modal.html($("#"+this.id+"-details").html());
+//   });
+//   var popupcon = $(".profile-popup-container");
+//   var popup    = $("#profile-popup");
+//   $(".profile-img").click(function(){
+//     popup.html($("#"+this.id+"-details").html());
+//     popupcon.removeClass("hidden");
+//   });
+//   popupcon.find(".close").click(function(){popupcon.addClass("hidden");});
+//   $(document).keyup(function(e){if (e.key === "Escape") {popupcon.addClass("hidden");}});
+// });
 $(window).on("load",function(){
   // exit the loader
   $(".load-content").removeClass("hidden");
