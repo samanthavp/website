2cc08b2fccd900f1fb28512e8336cd3914c6eec5
jessexknight <jesse.x.knight@gmail.com>
2019-08-18 10:19:40 -0400

    comment main.js, add hover overs on img-links

--- js/main.js
+++ js/main.js
@@ -4,11 +4,15 @@ $(window).on("load",function(){
   $(".loader").addClass("hidden");
 });
 
+// search
 var qregex = /[\?&]q=([^&]+)/g;
 var matches = qregex.exec(window.location.search);
 if (matches && matches[1]) {
+  // extract the search query
   var q = decodeURIComponent(matches[1].replace(/\+/g, "%20"));
+  // load the searchable content (episodes)
   $.getJSON("search/episodes.json").then(function(episodes) {
+    // fuse search options
     options = {
       keys: ["no","title","notes","authors","links"],
       id: "no",
@@ -19,18 +23,22 @@ if (matches && matches[1]) {
       shouldSort: true,
       threshold: 0.1,
     }
+    // do the search
     var fuse = new Fuse(episodes, options)
     var ids = fuse.search(q)
+    // hide episodes which don't match
     $(".tile-episode").each(function() {
       if (ids.includes($(this).attr("id")) === false) {
         $(this).hide();
       }
     })
+    // if no matches: show #no-search-results
     if (ids.length == 0) {
       $("#no-search-results").show();
     } else {
       $("#no-search-results").hide();
     }
+    // add back the searchbar with the query
     $(".season-header").hide();
     var searchbar = $("#searchbar")
     searchbar.addClass("show")
