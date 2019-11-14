3d5b330b0797dfcc4f749e328867b647beb8b395
jessexknight <jesse.x.knight@gmail.com>
2019-11-14 18:02:13 -0500

    adding parse script & first real content

---
+++ py/transcript.py
@@ -0,0 +1,26 @@
+import os
+import re
+import json
+from collections import OrderedDict as odict
+# file paths
+episode = 68
+ifile = os.path.join('resources','transcripts','raw',str(episode)+'.txt')
+ofile = os.path.join('src','content','transcripts',str(episode)+'.json')
+# source txt file - from otter.ai
+print('reading: {}'.format(ifile))
+with open(ifile,'r') as f:
+  raw = f.read()
+# parse the file
+print('parsing ...')
+data = []
+for match in re.findall('(.*)  (\d*:\d\d)  \n(.*)',raw):
+  print(' > [{}] {}'.format(match[1],match[0]))
+  data.append(odict([
+      ('speaker', match[0].replace('Unknown Speaker','')),
+      ('time', match[1]),
+      ('text', match[2]),
+    ]))
+# output file - warning: will overwrite existing json
+print('writing: {}'.format(ofile))
+with open(ofile,'w') as f:
+  json.dump(data,f,indent=2)
